04002724d933553729eb5c1afddbb52924a19ca7
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2016-10-03 16:53:31 +0200

    Release v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # vue-builder-webpack-plugin
+
 Webpack plugin to build vue files automatically
+
+Are you having issues with editors when working on vue files? This is for you
+
+Do you want to separate your `vue` file? This is for you
+
+You just don't like having the vue file? This is for you
+
+You can use this plugin to automatically generate `.vue` files.
+
+## Installation
+
+```
+$ npm install github.com/pksunkara/vue-builder-webpack-plugin --save-dev
+```
+
+## Usage
+
+In your `webpack.config.js`
+
+```js
+var VueBuilder = require('vue-builder-webpack-plugin');
+ 
+module.exports = {
+    // ... 
+    plugins: [
+      new VueBuilder({
+      	  path: 'src'
+      });
+    ]
+};
+```
+
+Whenever you run webpack in normal or watch mode, this plugin will automatically generate your `.vue` files.
+
+#### Options
+
+* `path`: The relative path to the folder which needs to be searched for to build vue files
+
+## Example
+
+If you have the following files, `App.vue.js`, `App.vue.css`, `App.vue.html`, the generated `App.vue` file will look like this:
+
+```vue
+<template src="./App.vue.html" lang="html"></template>
+<script src="./App.vue.js" lang="js"></script>
+<style src="./App.vue.css" lang="css"></style>
+```
+
+You can also rename the `App.vue.css` to `App.scoped.vue.css` to generate the following `style` tag:
+
+```vue
+<style src="./App.scoped.vue.css" lang="css" scoped></style>
+```
+
+This plugin inherently supports `jade`, `coffee`, `sass`, `scss`, `styl`, `less` as languages in vue file.
+
+If you like this project, please watch this and follow me.
+
+## Contributors
+Here is a list of [Contributors](http://github.com/pksunkara/vue-builder-webpack-plugin/contributors)
+
+### TODO
+
+__I accept pull requests and guarantee a reply back within a day__
+
+## License
+MIT/X11
+
+## Bug Reports
+Report [here](http://github.com/pksunkara/vue-builder-webpack-plugin/issues). __Guaranteed reply within a day__.
+
+## Contact
+Pavan Kumar Sunkara (pavan.sss1991@gmail.com)
+
+Follow me on [github](https://github.com/users/follow?target=pksunkara), [twitter](http://twitter.com/pksunkara)
---
+++ index.js
@@ -0,0 +1,137 @@
+var recursiveRead = require('recursive-readdir')
+  , path = require('path')
+  , fs = require('fs')
+  , directory = __dirname;
+
+function VueBuilderPlugin (options) {
+	directory = path.resolve(path.join(__dirname, options.path || ''));
+}
+
+function buildVues(callback) {
+  console.log('Building vue files');
+
+  recursiveRead(directory, function (err, files) {
+    if (err) {
+      return callback(err);
+    }
+
+    var vues = {}, sources = {
+      script: {},
+      template: {},
+      style: {}
+    };
+
+    function langCheck(file, extension, type) {
+      var length = -5 - extension.length;
+      var scoped = false;
+
+      if (file.slice(length) == '.vue.' + extension) {
+        var name = file.slice(0, length);
+
+        if (type == 'style' && name.slice(-7) == '.scoped') {
+          scoped = true;
+          name = name.slice(0, -7)
+        }
+
+        vues[name] = true;
+        sources[type][name] = {
+          file: file,
+          lang: extension
+        };
+
+        if (scoped) {
+          sources.style[name].scoped = true;
+        }
+
+        return true;
+      }
+
+      return false;
+    }
+
+    function singleVue(name) {
+      var data = '';
+
+      var script = sources.script[name];
+      var style = sources.style[name];
+      var template = sources.template[name];
+
+      data += '<script src="' + script.file + '" lang="' + script.lang + '"></script>\n';
+      data += '<style src="' + style.file + '" lang="' + style.lang + '"' + (style.scoped ? ' scoped' : '') + '></style>\n';
+      data += '<template src="' + template.file + '" lang="' + template.lang + '"></template>\n';
+
+      return data;
+    }
+
+    files.forEach(function (file) {
+      if (langCheck(file, 'html', 'template')) {
+        return;
+      }
+
+      if (langCheck(file, 'js', 'script')) {
+        return;
+      }
+
+      if (langCheck(file, 'css', 'style')) {
+        return;
+      }
+
+      // HTML alternatives
+      if (langCheck(file, 'jade', 'template')) {
+        return;
+      }
+
+      // JS alternatives
+      if (langCheck(file, 'coffee', 'script')) {
+        return;
+      }
+
+      // CSS alternatives
+      if (langCheck(file, 'sass', 'style')) {
+        return;
+      }
+
+      if (langCheck(file, 'scss', 'style')) {
+        return;
+      }
+
+      if (langCheck(file, 'less', 'style')) {
+        return;
+      }
+
+      if (langCheck(file, 'styl', 'style')) {
+        return;
+      }
+    });
+
+    Object.keys(vues).forEach(function (vue) {
+      if (sources.script[vue] && sources.style[vue] && sources.template[vue]) {
+        fs.writeFileSync(vue + '.vue', singleVue(vue, sources), 'utf8');
+      }
+    });
+
+    callback();
+  });
+}
+
+VueBuilderPlugin.prototype.apply = function(compiler) {
+  compiler.plugin('run', function (compilation, callback) {
+    buildVues(callback);
+  });
+
+  compiler.plugin('watch-run', function (compilation, callback) {
+    buildVues(callback);
+  });
+
+  compiler.plugin('after-compile', function (compilation) {
+    compilation.fileDependencies = compilation.fileDependencies.filter(function (file) {
+      if (file.slice(-4) == '.vue') {
+        return false;
+      }
+
+      return true;
+    });
+  });
+};
+
+module.exports = VueBuilderPlugin;
