9dd894d2a2f01dd56b98d97ca9d2b9d6b7fc6317
Quique Fdez Guerra <quique@ckgrafico.com>
2018-10-07 19:46:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var recursiveRead = require('recursive-readdir')
-  , path = require('path')
-  , fs = require('fs')
-  , directory = __dirname
-  , folder = false
-  , allScoped = false
-  , suffix = '';
+const recursiveRead = require('recursive-readdir');
+const path = require('path');
+const fs = require('fs');
 
-function VueBuilderPlugin (options) {
+let directory = __dirname;
+let folder = false;
+let allScoped = false;
+
+const VueBuilderPlugin = (options) => {
   if (path.isAbsolute(options.path)) {
     directory = options.path;
   } else {
@@ -16,50 +16,48 @@ function VueBuilderPlugin (options) {
   if (options.folder) {
     folder = true;
   }
-
+  
   if (options.allScoped) {
     allScoped = true;
   }
+};
 
-  if (options.suffix) {
-    suffix = options.suffix;
-  }
-}
-
-function buildVues(callback) {
+const buildVues = (callback) => {
+  // eslint-disable-next-line no-console
   console.log('Building vue files');
 
-  recursiveRead(directory, function (err, files) {
+  recursiveRead(directory, (err, files) => {
     if (err) {
       return callback(err);
     }
 
-    var vues = {}, sources = {
+    const vues = {};
+    const sources = {
       script: {},
       template: {},
-      style: {}
+      style: {},
     };
 
-    function langCheck(file, extension, type) {
-      var length = -5 - extension.length;
-      var scoped = false; 
-      if (file.slice(length) == '.vue.' + extension) {
+    const langCheck = (file, extension, type) => {
+      const length = -5 - extension.length;
+      let scoped = false;
 
-        var name = file.slice(0, length);
+      if (file.slice(length) === `.vue.${extension}`) {
+        let name = file.slice(0, length);
 
-        if (type == 'style' && name.slice(-7) == '.scoped') {
+        if (type === 'style' && name.slice(-7) === '.scoped') {
           scoped = true;
-          name = name.slice(0, -7)
+          name = name.slice(0, -7);
         }
-
+        
         if (type == 'style' && allScoped) {
            scoped = true;
         }
 
         vues[name] = true;
         sources[type][name] = {
-          file: file,
-          lang: extension
+          file,
+          lang: extension,
         };
 
         if (scoped) {
@@ -70,27 +68,25 @@ function buildVues(callback) {
       }
 
       return false;
-    }
+    };
 
-    function singleVue(name, dirname) {
-      var data = '';
+    const singleVue = (name, dirname) => {
+      let data = '';
 
-      var script = sources.script[name];
-      var style = sources.style[name];
-      var template = sources.template[name];
+      const script = sources.script[name];
+      const style = sources.style[name];
+      const template = sources.template[name];
 
-      function relate(file) {
-        return '.' + path.sep + path.relative(dirname, file);
-      }
+      const relate = file => `.${path.sep}${path.relative(dirname, file)}`;
 
-      data += '<script src="' + relate(script.file) + '" lang="' + script.lang + '"></script>\n';
-      data += '<style src="' + relate(style.file) + '" lang="' + style.lang + '"' + (style.scoped ? ' scoped' : '') + '></style>\n';
-      data += '<template src="' + relate(template.file) + '" lang="' + template.lang + '"></template>\n';
+      data += `<script src="${relate(script.file)}" lang="${script.lang}"></script>\n`;
+      data += `<style src="${relate(style.file)}" lang="${style.lang}"${style.scoped ? ' scoped' : ''}></style>\n`;
+      data += `<template src="${relate(template.file)}" lang="${template.lang}"></template>\n`;
 
       return data;
-    }
+    };
 
-    files.forEach(function (file) {
+    files.forEach((file) => {
       if (langCheck(file, 'html', 'template')) {
         return;
       }
@@ -134,39 +130,33 @@ function buildVues(callback) {
         return;
       }
 
-      if (langCheck(file, 'styl', 'style')) {
-        return;
-      }
+      langCheck(file, 'styl', 'style');
     });
 
-    Object.keys(vues).forEach(function (vue) {
-      var dest = vue;
+    Object.keys(vues).forEach((vue) => {
+      let dest = vue;
 
       if (folder && path.basename(vue) === path.basename(path.dirname(vue))) {
         dest = path.dirname(vue);
       }
 
       if (sources.script[vue] && sources.style[vue] && sources.template[vue]) {
-        fs.writeFileSync(dest + suffix + '.vue', singleVue(vue, path.dirname(dest)), 'utf8');
+        fs.writeFileSync(`${dest}.vue`, singleVue(vue, path.dirname(dest)), 'utf8');
       }
     });
 
-    callback();
+    return callback();
   });
-}
+};
 
-VueBuilderPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('run', function (compilation, callback) {
-    buildVues(callback);
-  });
+VueBuilderPlugin.prototype.apply = (compiler) => {
+  compiler.plugin('run', (compilation, callback) => buildVues(callback));
+  compiler.plugin('watch-run', (compilation, callback) => buildVues(callback));
 
-  compiler.plugin('watch-run', function (compilation, callback) {
-    buildVues(callback);
-  });
-
-  compiler.plugin('after-compile', function (compilation, callback) {
-    compilation.fileDependencies = Array.from(compilation.fileDependencies).filter(function (file) {
-      if (file.slice(-4) == '.vue') {
+  compiler.plugin('after-compile', (compilation, callback) => {
+    // eslint-disable-next-line no-param-reassign
+    compilation.fileDependencies = Array.from(compilation.fileDependencies).filter((file) => {
+      if (file.slice(-4) === '.vue') {
         return false;
       }
 
