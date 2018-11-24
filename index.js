b089eabc29365a930eb68827c8017255101603ea
Quique Fdez Guerra <quique@ckgrafico.com>
2018-11-24 22:12:46 +0100

    Use virtual files instead of physical files

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const recursiveRead = require('recursive-readdir');
 const path = require('path');
 const fs = require('fs');
+const VirtualModulePlugin = require('./virtual-module-webpack-plugin');
 
 let directory = __dirname;
 let folder = false;
 let allScoped = false;
 
-const VueBuilderPlugin = (options) => {
+function VueBuilderPlugin(options) {
   if (path.isAbsolute(options.path)) {
     directory = options.path;
   } else {
@@ -22,7 +23,7 @@ const VueBuilderPlugin = (options) => {
   }
 };
 
-const buildVues = (callback) => {
+const buildVues = (callback, compiler) => {
   // eslint-disable-next-line no-console
   console.log('Building vue files');
 
@@ -141,7 +142,12 @@ const buildVues = (callback) => {
       }
 
       if (sources.script[vue] && sources.style[vue] && sources.template[vue]) {
-        fs.writeFileSync(`${dest}.vue`, singleVue(vue, path.dirname(dest)), 'utf8');
+        const modulePath = `${dest}.vue`;
+        const ctime = VirtualModulePlugin.statsDate();
+        const contents = singleVue(vue, path.dirname(dest));
+        const _fs = (this && this.fileSystem) || compiler.inputFileSystem;
+
+        VirtualModulePlugin.populateFilesystem({ fs: _fs, modulePath, contents, ctime });
       }
     });
 
@@ -150,8 +156,8 @@ const buildVues = (callback) => {
 };
 
 VueBuilderPlugin.prototype.apply = (compiler) => {
-  compiler.plugin('run', (compilation, callback) => buildVues(callback));
-  compiler.plugin('watch-run', (compilation, callback) => buildVues(callback));
+  compiler.plugin('run', (compilation, callback) => buildVues(callback, compiler));
+  compiler.plugin('watch-run', (compilation, callback) => buildVues(callback, compiler));
 
   compiler.plugin('after-compile', (compilation, callback) => {
     // eslint-disable-next-line no-param-reassign
